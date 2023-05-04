d9ac9de6c80549955a2acbfac303b0ae14cf15ed
Flak DiNenno <flakd@users.noreply.github.com>
2023-05-04 16:26:47 -0400

    Add files via upload
    Moving main code to HangmanUI and using it as a wrapper for the app instead of HangmanModel

--- hangman_new.py
+++ hangman_new.py
@@ -14,7 +14,6 @@ class AnswerResponse:
   num_answers: int 
   response_msg: str
   was_successful: bool
-  
 
   def __setOtherDefaults(self,word_group):  
     self._num_answer_words = self.get_num_answer_words(word_group)
@@ -41,7 +40,6 @@ class AnswerResponse:
     return len(self.get_answer_words(word))
 
 
-
 class AnswerProvider:
   def __get_answerList(self) -> list[str]:
     answerList = ["ardvark", "baboon", "cougar", "dingo", "door hinge"]
@@ -51,7 +49,7 @@ class AnswerProvider:
     answer_list = self.__get_answerList()
     return random.choice(answer_list)  
   
-  def get_answer(self, searchTerm, hm_ui):
+  def get_answer(self, searchTerm:str) -> AnswerResponse:
     headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0'}
     url = "https://api.datamuse.com/words?rel_rhy=" + searchTerm + "&max=1000"
     answer_response = AnswerResponse()
@@ -95,7 +93,8 @@ class AnswerProvider:
             num_syllables_dict[answer['numSyllables']].append(answer["word"]) 
           num_syllables_choice = hm_ui.get_num_syllables_choice(max_syllables)        
           answers_to_choose_from = num_syllables_dict[num_syllables_choice]
-          answer_response.word_group = random.choice(answers_to_choose_from)      
+          word_group = random.choice(answers_to_choose_from)
+          answer_response.set_word_group(word_group)      
           message = "There are " + str(len(answers_to_choose_from)) + " word(s) with " + \
             str(num_syllables_choice) + " syllables that rhyme with " + searchTerm + ". I'll randomly select one for you"
 
@@ -114,24 +113,7 @@ class AnswerProvider:
     return answer_response      
 
 
-class HangmanUI:
-  def get_num_syllables_choice(self,max_syllables) -> int:
-    num_syllable_choice = ""
-    isInt = False
-    while not isInt: 
-      num_syllable_choice = input(f"\nI have found words ranging from 2 to {max_syllables} syllables. How large a word do you want to guess (2-{max_syllables})?").lower()
-      if Utils.is_string_an_int_between(num_syllable_choice,2,max_syllables):
-        isInt = True
-    return int(num_syllable_choice)
-  
-  def getSearchTerm(self) -> str:
-    searchTerm = ""
-    isString = False
-    while not isString: 
-      searchTerm = input("\nPick a rhyming word: ").lower()
-      if Utils.is_whole_word_alpha(searchTerm):
-        isString = True
-    return searchTerm    
+   
 
 
 class Utils:
@@ -169,63 +151,20 @@ class Utils:
 
 
 class HangmanModel:  
-  _hm_UI: HangmanUI
   _answer: AnswerResponse
   _guess_letter: str
   _answer_provider: AnswerProvider
   _guesses_remaining: int
   _answer_guess: list[str]
   
-  def __init__(self, answer_provider: AnswerProvider, hm_ui):
-    self._hm_UI = hm_ui
+  def __init__(self, answer_provider: AnswerProvider):
     self._guesses_remaining = 6
     self._answer_provider = answer_provider
-    
-  def start(self) -> None:
-    searchTerm = self._hm_UI.getSearchTerm()
-    self._answer = self._answer_provider.get_answer(searchTerm, self._hm_UI)
-    #self._answer = self._answer_provider.get_answer()
-    if self._answer.num_answers == 0:
-      print(self._answer.response_msg)
-      self.start()
-    else:
-      print(self._answer.response_msg)
-      print(self._answer.word_group)
-    self._answer_guess = len(self._answer.word_group) * [""]
-    while self._answer_guess != self._answer.word_group or self._guesses_remaining > 0:
-      self.__main_game_loop()
 
-  def __main_game_loop(self) -> None:
-    guess = self.__get_guess()
-    isCorrect = self.__is_guess_in_answer()
-    progressMsg = self.__update_game_state(isCorrect)
-    self.__report_progress(progressMsg)
-
-
-  
-  def __get_guess(self) -> str:
-    self._guess_letter = input("Please guess a letter: ")
-    pass
-
-  def __is_guess_in_answer(self) -> bool:
+  def is_guess_in_answer(self) -> bool:
     return self._guess_letter in self._answer.word_group
 
-  def __have_guessed_correct_before():
-    pass
-
-  def __have_guessed_incorrect_before():
-    pass
-
-  def __get_answer_guess_as_UI(self):
-    asUI = []
-    for el in self._answer_guess:
-      if el == "":
-        asUI.append("_")
-      else:
-        asUI.append(el)        
-    return " ".join(asUI)
-
-  def __update_game_state(self, isCorrect: bool) -> None:
+  def update_game_state(self, isCorrect: bool) -> None:
     if not self.__is_guess_in_answer():
       self._guesses_remaining -= 1
       message = "Wrong! You have " + str(self._guesses_remaining) + " guesses remaining."
@@ -236,14 +175,72 @@ class HangmanModel:
       message = "Updated Guess-Answer: " + self.__get_answer_guess_as_UI()
     return message
 
-  def __report_progress(self,progressMsg) -> None:
+
+
+
+
+class HangmanUI:
+  _hmm: HangmanModel
+  _ap: AnswerProvider
+
+  def start(self, hmm:HangmanModel) -> None:
+    searchTerm = self.get_search_term_UI()
+    answer = hmm.ap.get_answer(searchTerm)
+    if answer.num_answers == 0:
+      print(answer.response_msg)
+      self.start()
+    else:
+      print(answer.response_msg)
+      print(answer.get_word_group)
+    answer_guess = len(answer.get_word_group) * [""]
+    while answer_guess != answer.get_word_group or hmm._guesses_remaining > 0:
+      self.__main_game_loop(hmm, ap)
+
+  def __main_game_loop(self, hmm:HangmanModel, ap:AnswerProvider) -> None:
+    guess = self.__get_guess_UI()
+    isCorrect = hmm.is_guess_in_answer()
+    progressMsg = hmm.update_game_state(isCorrect)
+    self.__report_progress(progressMsg)
+
+  def __get_guess_UI(self) -> str:
+    self._guess_letter = input("Please guess a letter: ")
+
+  def __get_answer_guess_UI(self):
+    asUI = []
+    for el in self._answer_guess:
+      if el == "":
+        asUI.append("_")
+      else:
+        asUI.append(el)        
+    return " ".join(asUI)
+
+  def get_num_syllables_choice_UI(self,max_syllables) -> int:
+    num_syllable_choice = ""
+    isInt = False
+    while not isInt: 
+      num_syllable_choice = input(f"\nI have found words ranging from 2 to {max_syllables} syllables. How large a word do you want to guess (2-{max_syllables})?").lower()
+      if Utils.is_string_an_int_between(num_syllable_choice,2,max_syllables):
+        isInt = True
+    return int(num_syllable_choice)
+  
+  def get_search_term_UI(self) -> str:
+    searchTerm = ""
+    isString = False
+    while not isString: 
+      searchTerm = input("\nPick a rhyming word: ").lower()
+      if Utils.is_whole_word_alpha(searchTerm):
+        isString = True
+    return searchTerm 
+  
+  def __report_progress(self,progressMsg:str) -> None:
     print(progressMsg)
     pass 
 
+
 ap = AnswerProvider()
+hmm = HangmanModel(ap)
 hm_UI = HangmanUI()
-hm = HangmanModel(ap, hm_UI)
-hm.start()
+hm_UI.start(hmm, ap)
 
 
 
